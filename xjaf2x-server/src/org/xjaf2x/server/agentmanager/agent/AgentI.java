81e8835622484ba0d8e083e3297fc1d4f2f8d190
dejan <dejan@4e613658-a215-e311-a5a8-000c29d769ed>
2014-04-08 13:03:17 +0000

    xjaf2x-server licensed under the Apache License, Version 2.0

--- xjaf2x-server/src/org/xjaf2x/server/agentmanager/agent/AgentI.java
+++ xjaf2x-server/src/org/xjaf2x/server/agentmanager/agent/AgentI.java
@@ -1,3 +1,23 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one 
+ * or more contributor license agreements. See the NOTICE file 
+ * distributed with this work for additional information regarding 
+ * copyright ownership. The ASF licenses this file to you under 
+ * the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may 
+ * obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. 
+ * 
+ * See the License for the specific language governing permissions 
+ * and limitations under the License.
+ */
+
 package org.xjaf2x.server.agentmanager.agent;
 
 import java.io.Serializable;
@@ -5,7 +25,7 @@ import javax.ejb.Remote;
 import org.xjaf2x.server.messagemanager.fipaacl.ACLMessage;
 
 /**
- * Base interface for all agents.
+ * Remote interface for all agents.
  * 
  * @author <a href="mailto:mitrovic.dejan@gmail.com">Dejan Mitrovic</a>
  */
@@ -18,13 +38,14 @@ public interface AgentI extends Serializable
 
 	AID getAid();
 
-	void setAid(AID aid);
-
 	/**
-	 * The remaining methods are for internal purposes only. You should never directly call or
-	 * override any of them.
+	 * The remaining methods are for internal purposes only. You should never
+	 * directly call or override any of them.
 	 */
 
+	void setAid(AID aid);
+
 	void handleMessage(ACLMessage msg);
+
 	void processNextMessage();
 }
--- xjaf2x-server/src/org/xjaf2x/server/agents/jason/HelloJason.java
+++ xjaf2x-server/src/org/xjaf2x/server/agents/jason/HelloJason.java
@@ -1,3 +1,23 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one 
+ * or more contributor license agreements. See the NOTICE file 
+ * distributed with this work for additional information regarding 
+ * copyright ownership. The ASF licenses this file to you under 
+ * the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may 
+ * obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. 
+ * 
+ * See the License for the specific language governing permissions 
+ * and limitations under the License.
+ */
+
 package org.xjaf2x.server.agents.jason;
 
 import jason.asSyntax.Literal;
@@ -11,6 +31,11 @@ import org.xjaf2x.server.agentmanager.agent.Agent;
 import org.xjaf2x.server.agentmanager.agent.jason.JasonAgentI;
 import org.xjaf2x.server.messagemanager.fipaacl.ACLMessage;
 
+/**
+ * Work in progress.
+ *
+ * @author <a href="mitrovic.dejan@gmail.com">Dejan Mitrovic</a>
+ */
 @Stateful(name = "org_xjaf2x_server_agents_jason_HelloJason")
 @Remote(JasonAgentI.class)
 @Clustered
