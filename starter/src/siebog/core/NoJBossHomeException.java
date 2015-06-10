112dc5abde0833c68901dc6413f77a9c19a4a9f4
_gcvt <mitrovic.dejan@gmail.com>
2015-06-10 19:14:30 +0200

    Discontinuing the starter project. Improved builds.

--- starter/src/siebog/core/NoJBossHomeException.java
+++
@@ -1,8 +0,0 @@
-package siebog.core;
-
-@SuppressWarnings("serial")
-public class NoJBossHomeException extends IllegalStateException {
-	public NoJBossHomeException() {
-		super("Environment variable JBOSS_HOME not (properly) set.");
-	}
-}
--- starter/src/siebog/core/Setup.java
+++
@@ -1,5 +0,0 @@
-package siebog.core;
-
-public class Setup {
-
-}
