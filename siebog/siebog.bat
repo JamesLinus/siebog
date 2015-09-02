6ede92b3653d0a51ccbea9a4152c930546eca94d
_gcvt <mitrovic.dejan@gmail.com>
2015-09-02 10:31:45 +0200

    Added simple starter scripts (sh and bat)

---
+++ siebog/siebog.bat
@@ -0,0 +1,2 @@
+set JBOSS_HOME=%~dp0wildfly-9.x
+java -jar siebog.war %1
--- siebog/siebog.sh
+++ siebog/siebog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JBOSS_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/wildfly-9.x
-java -jar siebog.war
+java -jar siebog.war $@
